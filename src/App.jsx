25bbe6aadbfb46f835329cd92659d93a2dfadb08
Barbara-nunes <babisnunes14@gmail.com>
2023-09-26 10:43:21 -0300

    Enviando projeto para deploy

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom"
+
+import NavBar from "./components/NavBar"
+
+import "./App.css"
+
+
+function App() {
+  return (
+    <div className="app-container">
+      <NavBar />
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
